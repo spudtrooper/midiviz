6b364542dc589aa2ffd82668906fd6aa83dfbbb5
Jeff Palm <jeff@jeffpalm.com>
2023-10-19 23:00:08 -0400

    update Thu Oct 19 23:00:08 EDT 2023

--- main.py
+++ main.py
@@ -3,27 +3,34 @@ import sys
 import midiviz
 import squares
 
+def loop(clock, events, screen, start_time):
+  while True:
+      elapsed_time = clock.tick(60) / 1000.0  # converting to seconds to match MIDI timings
+      current_time = (pygame.time.get_ticks() - start_time) / 1000.0
+
+      o.each(screen, events, current_time, elapsed_time)
+
+      pygame.display.flip()
+
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              return
+              
 def run(midi_path, o):
     events, track_count = midiviz.load_event_data(midi_path)
     o.init(events, track_count)
     screen, clock = midiviz.start_game(midi_path, o.width(), o.height())
     start_time = pygame.time.get_ticks()
+    
+    loop(clock, events, screen, start_time)
 
-    while True:
-        elapsed_time = clock.tick(60) / 1000.0  # converting to seconds to match MIDI timings
-        current_time = (pygame.time.get_ticks() - start_time) / 1000.0
-
-        o.each(screen, events, current_time, elapsed_time)
-
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    pygame.quit()
+    sys.exit()
+                
 
 # main
 pygame.init()
-midi_path = 'example/20210906-BrunchFull.mid'
+# midi_path = 'example/20210906-BrunchFull.mid'
+midi_path = 'data/smoke2.mid'
 o = squares.Squares()
 run(midi_path, o)
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
-/Library/Frameworks/Python.framework/Versions/3.11/bin/manim -pql main.py MusicAnimation
+set -x
+version=$(ls -1 /Users/jeff/Library/Python | sort | tail -1)
+dir=$(dirname $(python3 -m site --user-base))
+#$dir/$version/bin/manim -pql main.py MusicAnimation
+/opt/homebrew/bin/manim -pql main.py MusicAnimation
+# /Library/Frameworks/Python.framework/Versions/3.11/bin/manim -pql main.py MusicAnimation
